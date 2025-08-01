dae1e769a5b5361098db63e1a6aafa994b79aac6
TeddyMuli <tmuli974@gmail.com>
2025-08-02 00:24:09 +0300

    add links

--- components/landing/Header.tsx
+++ components/landing/Header.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Logo from '../Logo';
+import Link from 'next/link';
 
 const Header = () => {
+  const links = [
+    { name: "Integrations", href: "/#integrations" },
+    { name: "Features", href: "/#features" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" },
+  ];
+
   return (
-    <div>
+    <div className='flex gap-4 items-center'>
       <Logo />
+      <div className='flex gap-4'>
+        {links.map((link) => (
+          <Link key={link.name} href={link.href}>{link.name}</Link>
+        ))}
+      </div>
     </div>
   );
 }

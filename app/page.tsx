8c8efefb0de83886c0d87ce6c263e419d40bcc50
TeddyMuli <tmuli974@gmail.com>
2025-08-02 12:27:11 +0300

    complete hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+import JoinWaitlist from "@/components/landing/JoinWaitlist";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
-
+      <div className="flex flex-col md:flex-row gap-8 py-8">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-5xl font-bold">All your data.<br />One intelligent dashboard.</h1>
+          <p className="text-2xl max-w-[1200px] mt-auto">
+            Graff unifies your ad analytics across multiple platforms
+            into a single powerful dashboard helping you optimize performance and drive business growth.
+          </p>
+          <div>
+            <JoinWaitlist />
+          </div>
+        </div>
+        <div>
+          <Image
+            src="/assets/analytics.png"
+            alt="analytics image"
+            width={1000}
+            height={1000}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- components/landing/JoinWaitlist.tsx
+++ components/landing/JoinWaitlist.tsx
@@ -3,9 +3,9 @@ import { LuMoveRight } from 'react-icons/lu';
 
 const JoinWaitlist = () => {
   return (
-    <button className='flex group gap-2 items-center bg-black p-3 text-white rounded-xl hover:rounded-none delay-500'>
+    <button className='flex group gap-2 items-center bg-black p-3 text-white rounded-xl hover:rounded-none transition-all'>
       Join Waitlist
-      <LuMoveRight className='hidden group-hover:block transition-all delay-500' />
+      <LuMoveRight className='hidden group-hover:block transition-all delay-300' />
     </button>
   );
 }

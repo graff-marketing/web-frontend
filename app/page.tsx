755f818023e18e60d6d2c21f7e636d5a2aa6db23
TeddyMuli <tmuli974@gmail.com>
2025-08-02 13:19:41 +0300

    add quote

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import JoinWaitlist from "@/components/landing/JoinWaitlist";
 import Image from "next/image";
+import { LuQuote } from "react-icons/lu";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
+      {/** Hero Section */}
       <div className="flex flex-col md:flex-row gap-8 py-8">
         <div className="flex flex-col gap-4">
           <h1 className="text-5xl font-bold">All your data.<br />One intelligent dashboard.</h1>
@@ -23,6 +25,35 @@ export default function Home() {
             height={1000}
           />
         </div>
+      </div>
+
+      {/** Quote */}
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center gap-4">
+          <LuQuote size={64} />
+          <p className="text-3xl font-semibold">What gets measured gets managed.</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="flex w-[64px] h-[64px] rounded-full">
+            <Image
+              src="/assets/peter_drucker.jpg"
+              alt="image of peter drucker"
+              width={100}
+              height={100}
+              className="rounded-full"
+            />
+          </div>
+          <div>
+            <p className="font-semibold text-xl">Peter Drucker</p>
+            <p className="text-sm">Austrian-American<br/>consultant and educator</p>
+          </div>
+        </div>
+      </div>
+
+      {/** Features */}
+      <div>
+        <h1>Features</h1>
+        <p>Graff unifies your ad performance from platforms like Facebook, Instagram, TikTok, and Google — helping you uncover insights, cut waste, and grow faster.</p>
       </div>
     </div>
   );

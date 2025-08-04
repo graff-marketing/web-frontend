85f9ca987b51fc44ebfcee215c6a38a984623695
TeddyMuli <tmuli974@gmail.com>
2025-08-04 12:25:46 +0300

    add privacy policy

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,27 +3,6 @@ import React from 'react';
 const Page = () => {
   return (
     <div>
-      <h1>Privacy Policy</h1>
-      <p>Effective Date: August 04 2025</p>
-      <p>Graff ("we", "our", or "us") respects your privacy. This Privacy Policy outlines how we collect, use, disclose, and protect your personal information when you use our website and services.</p>
-      <h2>1. Information We Collect:</h2>
-      <ul>
-        <li>Personal Information: Name, email, contact details when you sign up.</li>
-        <li>Usage Data: Device info, browser type, pages visited, and other analytics.</li>
-        <li>Ad Platform Data: With your consent, we collect data from platforms like Meta, Google, and X via their APIs.</li>
-      </ul>
-      <h2>2. How We Use Your Information:</h2>
-      <ul>
-        <li>To provide and maintain our services.</li>
-        <li>To analyze and improve user experience.</li>
-        <li>To offer tailored insights and campaign recommendations.</li>
-      </ul>
-      <h2>3. Sharing Your Data: We do not sell your data. <p>We only share with trusted third-party services essential for analytics, storage, or legal obligations.</p></h2>
-      <h2>4. Your Rights: <p>You can request access, correction, or deletion of your data at any time.</p></h2>
-      <h2>5. Security: <p>We implement industry-standard security to protect your data.</p></h2>
-      <h2>6. Changes to This Policy: <p>We may update this policy periodically. You'll be notified of significant changes.</p></h2>
-
-      <p>Contact Us: <span>support@graff.co.ke</span></p>
     </div>
   );
 }

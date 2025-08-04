803aa7a1a5fe9473b346910327ee1cca5f70686d
TeddyMuli <tmuli974@gmail.com>
2025-08-04 15:02:18 +0300

    add waitlist link

--- components/landing/JoinWaitlist.tsx
+++ components/landing/JoinWaitlist.tsx
@@ -4,14 +4,16 @@ import { LuMoveRight } from 'react-icons/lu';
 const JoinWaitlist = () => {
   return (
     <div>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://forms.gle/K7zLquapf37ZgZjq9'
-        className='flex group gap-2 items-center bg-black p-3 text-white rounded-xl hover:rounded-none transition-all'>
-        Join Waitlist
-        <LuMoveRight className='hidden group-hover:block transition-all delay-300' />
-      </a>
+      <button>
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://forms.gle/K7zLquapf37ZgZjq9'
+          className='flex group gap-2 items-center bg-black p-3 text-white rounded-xl hover:rounded-none transition-all'>
+          Join Waitlist
+          <LuMoveRight className='hidden group-hover:block transition-all delay-300' />
+        </a>
+      </button>
     </div>
   );
 }
